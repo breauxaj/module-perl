6674ad7e12d903a1152a3c75cb9c5d95ae493a8d
Adam Breaux <breauxaj@gmail.com>
2017-03-30 18:00:18 -0400

    works

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -1,4 +1,15 @@
 node default {
   include ::perl
 
+  case $::osfamily {
+    'Debian': {
+      perl::modules { 'libxs-object-magic-perl': }
+    }
+    'RedHat': {
+      perl::modules { 'perl-libwww-perl': }  
+    }
+    default: {
+
+    }
+  }
 }
