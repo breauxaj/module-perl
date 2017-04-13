552fb1e379603becc5fcecdc142dd1a07c003913
Adam Breaux <breauxaj@gmail.com>
2017-04-13 18:56:52 -0400

    Added amazon, no errors

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -1,6 +1,13 @@
 node default {
   include ::perl
 
+  case $::operatingsystem {
+    'Amazon': {
+        Package { allow_virtual => false }
+    }
+    default: {}
+  }
+
   case $::osfamily {
     'Debian': {
       perl::modules { 'libxs-object-magic-perl': }
